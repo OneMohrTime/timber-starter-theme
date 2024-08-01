f38b88c843c1cd20ae1ebb5f582ec65f1df9a4a9
Derek Mohr <derek@onemohrti.me>
2024-08-01 15:38:00 -0400

    [#4] Add FitText as ES6 module

--- wp-content/themes/timber-starter-theme/src/scripts/modules.js
+++ wp-content/themes/timber-starter-theme/src/scripts/modules.js
@@ -8,6 +8,7 @@
 // =============================================================================
 // export { default as Example } from './modules/Example';
 export { default as Fancybox } from './modules/Fancybox';
+export { default as FitText } from './modules/FitText';
 // export { default as Load } from './modules/Load';
 export { default as Navigation } from './modules/Navigation';
 // export { default as Scroll } from './modules/Scroll';
