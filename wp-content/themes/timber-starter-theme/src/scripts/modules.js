7324f8ecbce113934287abe517cf78d8f647ae40
Derek Mohr <derek@onemohrti.me>
2025-05-30 23:52:50 -0400

    Turn off page loading animations

--- wp-content/themes/timber-starter-theme/src/scripts/modules.js
+++ wp-content/themes/timber-starter-theme/src/scripts/modules.js
@@ -9,7 +9,7 @@
 // export { default as Example } from './modules/Example';
 export { default as Fancybox } from './modules/Fancybox';
 export { default as FitText } from './modules/FitText';
-export { default as Load } from './modules/Load';
+// export { default as Load } from './modules/Load';
 export { default as Modal } from './modules/Modal';
 export { default as Navigation } from './modules/Navigation';
 export { default as Scroll } from './modules/Scroll';
